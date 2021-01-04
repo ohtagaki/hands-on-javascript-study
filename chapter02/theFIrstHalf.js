6c803bf8328c07d5e163bb89fe84587696bdefe2
ohtagaki <sweetlife0815hiikor@gmail.com>
2021-01-05 01:23:37 +0900

    study by section2-4

---
+++ chapter02/theFIrstHalf.js
@@ -0,0 +1,44 @@
+//2-1 データの種類
+console.log("9.80665"[2])
+
+console.log("9.865"[3])
+
+console.log(9.80665[2])
+
+//データ型
+//2-2 リテラル
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+console.log(typeof true)
+
+console.log(typeof 35)
+
+console.log(typeof 35n)
+
+console.log(typeof "文字列")
+
+console.log(typeof Symbol("ただの一般人"))
+
+console.log(typeof Math)
+
+//2-3 定数
+const pi = 3.14
+console.log((3 * 2) * pi)
+console.log((3 * 3) * pi)
+
+//2-4 変数
+console.log("2-4のソースコード")
+//初期化
+let radius = 6378
+//初期化は必須ではない
+let triangle
+console.log(radius)
+console.log(triangle)
+
+//値を代入
+radius = 125.00
+console.log(radius)
+radius = radius + 1.5
+console.log(radius)
