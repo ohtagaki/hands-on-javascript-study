58b572f09dae2109b5ce56b4374124b4221a9ca2
ohtagaki <sweetlife0815hiikor@gmail.com>
2021-06-30 00:54:14 +0900

    section2 finished

---
+++ javascript-essence-tutorial/010_PrerequisiteKnowledge/010_Variable/start/main.js
@@ -0,0 +1,11 @@
+let a = 0;
+let word = 'hello';
+
+const b = "bye";
+
+console.log(a);
+console.log(word);
+console.log(b);
+
+
+
---
+++ javascript-essence-tutorial/010_PrerequisiteKnowledge/020_Function/start/main.js
@@ -0,0 +1,7 @@
+let sampleName ='Mike';
+
+function sayhello(name){
+    console.log('hello ' + name);
+}
+
+sayhello(sampleName);
